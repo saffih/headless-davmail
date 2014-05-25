44588f366b634c619f5aba211dd96df7870da361
Saffi Hartal <saffi@il.sharedbook.com>
2014-05-25 18:32:50 +0300

    .

--- fix/davmail.sh
+++ fix/davmail.sh
@@ -9,11 +9,11 @@ for i in $BASE/lib/*; do export CLASSPATH=$CLASSPATH:$i; done
 TOEXE="exec java -Xmx512M
  -Dsun.net.inetaddr.ttl=60 -cp $BASE/davmail.jar:$CLASSPATH davmail.DavGateway $1"
 PROPS=$1
-echo ============= call ==============
-echo $TOEXE
 echo ============= props =============
 cat $PROPS
+echo ============= call ==============
+echo $TOEXE
 echo =================================
 $TOEXE
 echo =================================
-
+echo =================================
